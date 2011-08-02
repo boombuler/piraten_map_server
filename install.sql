ae1597f4651965deba79c9efe316a0a9eed8429f
boombuler <boombuler@googlemail.com>
2011-08-02 18:46:33 +0200

    added the required database tables as sql script

---
+++ install.sql
@@ -0,0 +1,41 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+CREATE TABLE IF NOT EXISTS plakate_felder (
+  id int(11) NOT NULL auto_increment,
+  lat double NOT NULL,
+  lon double NOT NULL,
+  `type` varchar(50) collate utf8_unicode_ci NOT NULL default 'default',
+  del int(1) NOT NULL default '0',
+  `user` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `comment` text collate utf8_unicode_ci NOT NULL,
+  image varchar(150) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS plakate_regions (
+  id int(11) NOT NULL auto_increment,
+  category varchar(50) character set latin1 NOT NULL,
+  lat double NOT NULL,
+  lon double NOT NULL,
+  zoom int(11) NOT NULL,
+  PRIMARY KEY  (id)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS plakate_users (
+  id int(11) NOT NULL auto_increment,
+  username varchar(50) collate utf8_unicode_ci NOT NULL,
+  `password` varchar(50) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (id)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
