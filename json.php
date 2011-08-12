b7715aa71ad6fa2d87b151a4fb1ca618ec209192
boombuler <boombuler@googlemail.com>
2011-08-12 22:14:33 +0200

    fixed several sql injections

--- json.php
+++ json.php
@@ -28,29 +28,28 @@ if ($loginok!=0)
 	switch ($_GET['action'])
 	{
 	case 'add':
-		$lon = preg_replace("/,/",".",$_GET['lon']);
-		$lat = preg_replace("/,/",".",$_GET['lat']);
-		$typ = preg_replace("/,/",".",$_GET['typ']);
+		$lon = mysql_real_escape_string(preg_replace("/,/",".",$_GET['lon']));
+		$lat = mysql_real_escape_string(preg_replace("/,/",".",$_GET['lat']));
+		$typ = mysql_real_escape_string(preg_replace("/,/",".",$_GET['typ']));
 		if ($typ != '')
 		  $res = mysql_query("INSERT INTO ".$tbl_prefix."felder (lon,lat,user,type) VALUES ('".$lon."','".$lat."','".$_SESSION['siduser']."', '".$typ."');") OR DIE("Database ERROR");
 		else
 		  $res = mysql_query("INSERT INTO ".$tbl_prefix."felder (lon,lat,user) VALUES ('".$lon."','".$lat."','".$_SESSION['siduser']."');") OR DIE("Database ERROR");
 	case 'del':
-		//$res = mysql_db_query("piraten", "DELETE FROM felder WHERE id = '".$_GET['id']."' LIMIT 1") OR DIE("Database ERROR");
-		$res = mysql_query("UPDATE ".$tbl_prefix."felder SET del='1',user='".$_SESSION['siduser']."' WHERE id = '".$_GET['id']."'") OR DIE("Database ERROR");
+		$res = mysql_query("UPDATE ".$tbl_prefix."felder SET del='1',user='".$_SESSION['siduser']."' WHERE id = '".mysql_real_escape_string($_GET['id'])."'") OR DIE("Database ERROR");
 	case 'change':
-		$type = $_GET['type'];
+		$type = mysql_real_escape_string($_GET['type']);
 		if (isset($options[$type]))
 		{
-			$res = mysql_query("UPDATE ".$tbl_prefix."felder SET type='".$type."',user='".$_SESSION['siduser']."' WHERE id = '".$_GET['id']."'") OR DIE("Database ERROR");
+			$res = mysql_query("UPDATE ".$tbl_prefix."felder SET type='".$type."',user='".$_SESSION['siduser']."' WHERE id = '".mysql_real_escape_string($_GET['id'])."'") OR DIE("Database ERROR");
 		}
 	case 'addcomment':
 		$comment = $_GET['comment'];
-		$comment = htmlentities($comment);
+		$comment = mysql_real_escape_string(htmlentities($comment));
 		$image   = $_GET['image'];
-		$image   = htmlentities($image);
+		$image   = mysql_real_escape_string(htmlentities($image));
 		//print $comment;	
-		$res = mysql_query( "UPDATE ".$tbl_prefix."felder SET comment='".$comment."',user='".$_SESSION['siduser']."', image='".$image."' WHERE id = '".$_GET['id']."'") OR DIE("Database ERROR");
+		$res = mysql_query( "UPDATE ".$tbl_prefix."felder SET comment='".$comment."',user='".$_SESSION['siduser']."', image='".$image."' WHERE id = '".mysql_real_escape_string($_GET['id'])."'") OR DIE("Database ERROR");
 	}
 }
 $rs = mysql_query("SELECT id,lon,lat,type,user,timestamp,comment,image FROM ".$tbl_prefix."felder WHERE del!='1' ORDER BY timestamp ASC") OR DIE("Database ERROR");
