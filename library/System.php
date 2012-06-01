53329c3fe80a0476302abfd96bcf97c647772b6e
Florian Sundermann <boombuler@googlemail.com>
2012-06-01 08:56:31 +0200

    applied new coding style to some classes

--- library/System.php
+++ library/System.php
@@ -1,129 +1,135 @@
-<?php
-session_start();
-
+﻿<?php
 /**
  * Singleton containing system whide important information.
  */
 class System
 {
-  /**
-   * @var bool
-   */
-  private static $initiated = false;
+    /**
+     * @var bool
+     * @static
+     */
+    private static $initiated = false;
 
-  private static $configuration = array();
+    /**
+     * @var array contains the current system settings
+     * @static
+     */
+    private static $configuration = array();
 
-  public static function init()
-  {
-    if (self::$initiated) {
-      return;
+    public static function init()
+    {
+        if (self::$initiated) {
+            return;
+        }
+
+        session_start();
+        spl_autoload_register('System::autoload');
+        setlocale(LC_ALL, 'de_DE.UTF-8');
+        self::readConfiguration();
+        self::$initiated = true;
     }
 
-    spl_autoload_register('System::autoload');
-    setlocale(LC_ALL, 'de_DE.UTF-8');
-    self::readConfiguration();
-    self::$initiated = true;
-  }
+    public static function autoload($classname)
+    {
+        $path = dirname(__FILE__) . '/' . str_replace('_', '/', $classname) . '.php';
+        if (!file_exists($path)) {
+            throw new Exception('Class ' . $classname . ' not Found');
+        }
 
-  public static function autoload($classname)
-  {
-    $path = dirname(__FILE__) . '/' . str_replace('_', '/', $classname) . '.php';
-    if (!file_exists($path)) {
-      throw new Exception('Class ' . $classname . ' not Found');
+        include_once($path);
     }
-
-    include_once($path);
-  }
   
-  private static function getDB() 
-  {
-    static $dbh = null;
-    if ($dbh == null)
+    private static function getDB() 
+    {
+        static $dbh = null;
+        if ($dbh == null) {
+            try {
+                $server = self::getConfig('mysql_server');
+                $database = self::getConfig('mysql_database');
+                $dbh = new PDO("mysql:host=$server;dbname=$database", 
+                    self::getConfig('mysql_user'), 
+                    self::getConfig('mysql_password'), 
+                    array(
+                        PDO::ATTR_PERSISTENT => true,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                    ));
+                $dbh->exec('SET CHARACTER SET utf8');
+                $dbh->exec('SET character_set_connection = utf8');
+                $dbh->exec('SET character_set_results = utf8');
+                $dbh->exec('SET character_set_client = utf8');
+            } catch (PDOException $e) {
+                if (self::getConfig('debug'))
+                    die("Error!: " . $e->getMessage() . "<br/>");
+                die();
+            }
+        }
+        return $dbh;
+    }
+
+    private static function readConfiguration()
+    {
+        include 'settings.php';
+
+        self::$configuration = get_defined_vars();
+    }
+
+    public static function getConfig($varname)
+    {
+        return self::$configuration[$varname];
+    }
+
+    public static function query($query, $arguments = null)
     {
         try {
-            $server = self::getConfig('mysql_server');
-            $database = self::getConfig('mysql_database');
-            $dbh = new PDO("mysql:host=$server;dbname=$database", self::getConfig('mysql_user'), self::getConfig('mysql_password'), array(
-                PDO::ATTR_PERSISTENT => true,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            ));
-            $dbh->exec("SET CHARACTER SET utf8");
-            $dbh->exec("SET character_set_connection = utf8");
-            $dbh->exec("SET character_set_results = utf8");
-            $dbh->exec("SET character_set_client = utf8");
-        } catch (PDOException $e) {
-            if (self::getConfig('debug'))
-                die("Error!: " . $e->getMessage() . "<br/>");
-            die();
+            $db = self::getDB();
+            $qry = $db->prepare($query);
+            $qry->execute($arguments);
+            return $qry;
+        } catch (Exception $e) {
+            if (self::getConfig('debug')) {
+                print $query;
+            }
+            throw $e;
         }
     }
-    return $dbh;
-  
-  }
 
-  private static function readConfiguration()
-  {
-    include 'settings.php';
+    public static function prepare($query)
+    {
+        return self::getDB()->prepare($query);
+    }
 
-    self::$configuration = get_defined_vars();
-  }
+    public static function lastInsertId()
+    {
+        return self::getDB()->lastInsertId();
+    }
 
-  public static function getConfig($varname)
-  {
-    return self::$configuration[$varname];
-  }
-
-  public static function query($query, $arguments = null)
-  {
-    try {
-        $db = self::getDB();
-        $qry = $db->prepare($query);
-        $qry->execute($arguments);
-        return $qry;
-    } catch (Exception $e) {
-        if (self::getConfig('debug')) {
-            print $query;
+    public static function getPosterFlags($key = null) 
+    {
+        $flags = self::getConfig('poster_flags');
+        if ($key == null) {
+            return $flags;
+        } else {
+            return $flags[$key];
         }
-        throw $e;
     }
-  }
 
-  public static function prepare($query)
-  {
-    return self::getDB()->prepare($query);
-  }
-
-  public static function lastInsertId()
-  {
-    return self::getDB()->lastInsertId();
-  }
-
-  public static function getPosterFlags($key = null) {
-    $flags = self::getConfig('poster_flags');
-    if ($key == null) {
-        return $flags;
-    } else {
-        return $flags[$key];
+    public static function getUserData() 
+    {
+        $usertyp = $_SESSION['sidusertype'];
+        if (!isset($usertyp))
+            return null;
+        return array(
+            'username' => $_SESSION['siduser'],
+            'usertype' => $usertyp,
+            'admin' => isAdmin()
+        );
     }
-  }
 
-  public static function getUserData() {
-      $usertyp = $_SESSION['sidusertype'];
-      if (!isset($usertyp))
-          return null;
-      return array(
-          'username' => $_SESSION['siduser'],
-          'usertype' => $usertyp,
-          'admin' => isAdmin()
-      );
-  }
-
-  public static function canSendMails() {
-     return self::getConfig('send_mail_adr') != '';
-  }
+    public static function canSendMails() 
+    {
+        return self::getConfig('send_mail_adr') != '';
+    }
 }
 
 System::init();
-
-?>
--- library/WikiConnection.php
+++ library/WikiConnection.php
@@ -1,58 +1,83 @@
-<?php
+﻿<?php
 require_once(dirname(__FILE__). '/System.php');
-
+/**
+ * Encapsulates the communication with the wiki system.
+ */
 class WikiConnection
 {
+    /**
+     * @var Snoopy
+     */
     private $snoopy = null;
-    private $apipath = "";
+    /**
+     * @var string
+     */
+    private $apipath = '';
+    /**
+     * @var mixed
+     */
     private $wiki_session = false;
 
-    public static function getInstance() {
+    public static function getInstance() 
+    {
         if (!$_SESSION['wikiconnection'] instanceof WikiConnection)
             $_SESSION['wikiconnection'] = new WikiConnection;
         return $_SESSION['wikiconnection'];
     }
 
-    private function __construct() {
+    private function __construct() 
+    {
         $this->snoopy = new Snoopy();
 
         if (System::getConfig('use_ssl')) {
             $this->snoopy->curl_path = System::getConfig('curl_path');
-            $wikiPath = "https://wiki.piratenpartei.de";
+            $wikiPath = 'https://wiki.piratenpartei.de';
         } else {
             $this->snoopy->curl_path = false;
-            $wikiPath = "http://wiki.piratenpartei.de";
+            $wikiPath = 'http://wiki.piratenpartei.de';
         }
-        $this->apiPath = "$wikiPath/wiki/api.php";
+        $this->apiPath = $wikiPath . '/wiki/api.php';
     }
 
-    public static function login($username, $password) {
+    public static function login($username, $password) 
+    {
         $instance = self::getInstance();
         // Make sure that the first username char is uppercase:
         $username = strtoupper(substr($username, 0, 1)) . substr($username, 1, strlen($username) - 1);
 
-        $request_vars = array('action' => 'login', 'lgname' => $username, 'lgpassword' => $password, 'format' => 'php');
+        $request_vars = array(
+            'action' => 'login', 
+            'lgname' => $username, 
+            'lgpassword' => $password, 
+            'format' => 'php'
+        );
         if (!$instance->snoopy->submit($instance->apiPath, $request_vars))
-            die("Snoopy error: {$instance->snoopy->error}");
+            die('Snoopy error: ' . $instance->snoopy->error);
         // We're only really interested in the cookies
         $instance->snoopy->setcookies();
         $array = unserialize($instance->snoopy->results);
 
-        if ($array['login']['result'] == "NeedToken") {
-            $request_vars = array('action' => 'login', 'lgname' => $username, 'lgpassword' => $password, 'lgtoken' => $array['login']['token'], 'format' => 'php');
+        if ($array['login']['result'] == 'NeedToken') {
+            $request_vars = array(
+                'action' => 'login', 
+                'lgname' => $username, 
+                'lgpassword' => $password, 
+                'lgtoken' => $array['login']['token'], 
+                'format' => 'php'
+            );
             if (!$instance->snoopy->submit($instance->apiPath, $request_vars))
-                die("Snoopy error: {$instance->snoopy->error}");
+                die('Snoopy error: ' . $instance->snoopy->error);
 
             // We're only really interested in the cookies
             $instance->snoopy->setcookies();
             $array = unserialize($instance->snoopy->results);
         }
 
-        if ($array['login']['result'] == "Success") {
-              //TODO: move the next two lines to a user object:
+        if ($array['login']['result'] == 'Success') {
+              //TODO: move the next three lines to a user object:
               $_SESSION['siduser'] = $username;
-              $_SESSION['sidip'] = $_SERVER["REMOTE_ADDR"];
-              $_SESSION['sidusertype'] = "wiki";
+              $_SESSION['sidip'] = $_SERVER['REMOTE_ADDR'];
+              $_SESSION['sidusertype'] = 'wiki';
               $instance->wiki_session = $instance->snoopy->cookies;
 
               return true;
@@ -60,36 +85,51 @@ class WikiConnection
         return false;
     }
 
-    public static function logout() {
+    public static function logout() 
+    {
         $instance = self::getInstance();
         if ($instance->wiki_session) {
             $instance->snoopy->cookies = $instance->wiki_session;
-            $request_vars = array('action' => 'logout', 'format' => 'php');
+            $request_vars = array(
+                'action' => 'logout', 
+                'format' => 'php'
+            );
             if (!$instance->snoopy->submit($instance->apiPath, $request_vars))
-                die("Snoopy error: {$instance->snoopy->error}");
+                die('Snoopy error: ' . $instance->snoopy->error);
             unset($_SESSION['wikiconnection']); // drop current instance
         }
     }
 
-    public static function isSessionValid() {
+    public static function isSessionValid() 
+    {
         $instance = self::getInstance();
         if (!$instance->wiki_session)
             return true; // No session is a valid session.
 
         $instance->snoopy->cookies = $instance->wiki_session;
 
-        $request_vars = array('action' => 'query', 'meta' => 'userinfo',  'format' => 'php');
+        $request_vars = array(
+            'action' => 'query', 
+            'meta' => 'userinfo',  
+            'format' => 'php'
+        );
         if(!$instance->snoopy->submit($instance->apiPath, $request_vars))
-            die("Snoopy error: {$instance->snoopy->error}");
+            die('Snoopy error: ' . $instance->snoopy->error);
         $array = unserialize($instance->snoopy->results);
         return $_SESSION['siduser'] == $array['query']['userinfo']['name'];
     }
 
-    public static function getUserCategories() {
+    public static function getUserCategories() 
+    {
         $instance = self::getInstance();
         $categories = array('Germany');
 
-        $request_vars = array('action' => 'query', 'prop' => 'categories', 'titles' => 'Benutzer:' . $_SESSION['siduser'], 'format' => 'php');
+        $request_vars = array(
+            'action' => 'query', 
+            'prop' => 'categories', 
+            'titles' => 'Benutzer:' . $_SESSION['siduser'], 
+            'format' => 'php'
+        );
 
         if ($instance->snoopy->submit($instance->apiPath, $request_vars)) {
             $array = unserialize($instance->snoopy->results);
@@ -111,5 +151,3 @@ class WikiConnection
         return $categories;
     }
 }
-
-?>
